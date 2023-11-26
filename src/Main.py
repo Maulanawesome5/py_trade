00380e7108534855bc3bf5069fd3da145a0f7dc7
maulanawesome5 <maulanawesome5@gmail.com>
2023-11-26 10:38:59 +0700

    Update class menubar

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, filedialog, messagebox
 
 
 class App(tk.Tk):
@@ -48,34 +48,54 @@ class Menubar(tk.Menu):
         super().__init__(parent)
         self.create_widget()
 
+    def openfile(self):
+        """
+        Function untuk mengakses dataframe di local computer. Support file `.xlsx` `.csv`
+        * 2023/11/26 : Belum dibuatkan proses
+        """
+        path = r".\\data\\"
+        self.filename = filedialog.askopenfilename(initialdir=path,
+                                                   title="Open DataFrame...",
+                                                   filetypes=(("Excel Workbook", ".xlsx"), ("CSV", ".csv"))
+                                                   )
+    
+    def popup(self):
+        """
+        Function untuk menampilkan interaksi berupa messagebox / dialogbox.
+        * 2023/11/26 : Belum dibuatkan proses
+        """
+        info_title = "Perhatian"
+        info_message = "Maaf. Menu masih dalam tahap konstruksi, kembali lain waktu."
+        response = messagebox.showinfo(info_title, info_message)
+
     def create_widget(self):
         """Function untuk membuat komponen Menu bar."""
         # Menu file
         menu_file = tk.Menu(self, tearoff=0)
-        menu_file.add_command(label="Open File...")
-        menu_file.add_command(label="Save")
-        menu_file.add_command(label="Preference")
+        menu_file.add_command(label="Open File...", command=self.openfile)
+        menu_file.add_command(label="Save", command=self.popup)
+        menu_file.add_command(label="Preference", command=self.popup)
         menu_file.add_command(label="Exit", command=self.quit)
 
         # Menu edit
         menu_edit = tk.Menu(self, tearoff=0)
-        menu_edit.add_command(label="Terserah")
-        menu_edit.add_command(label="Mau Ngapain")
+        menu_edit.add_command(label="Terserah", command=self.popup)
+        menu_edit.add_command(label="Mau Ngapain", command=self.popup)
 
         # Menu selection
         menu_selection = tk.Menu(self, tearoff=0)
-        menu_selection.add_command(label="Terserah")
-        menu_selection.add_command(label="Mau Ngapain")
+        menu_selection.add_command(label="Terserah", command=self.popup)
+        menu_selection.add_command(label="Mau Ngapain", command=self.popup)
 
         # Menu view
         menu_view = tk.Menu(self, tearoff=0)
-        menu_view.add_command(label="Terserah")
-        menu_view.add_command(label="Mau Ngapain")
+        menu_view.add_command(label="Terserah", command=self.popup)
+        menu_view.add_command(label="Mau Ngapain", command=self.popup)
 
         # Menu help
         menu_help = tk.Menu(self, tearoff=0)
-        menu_help.add_command(label="Terserah")
-        menu_help.add_command(label="Mau Ngapain")
+        menu_help.add_command(label="Terserah", command=self.popup)
+        menu_help.add_command(label="Mau Ngapain", command=self.popup)
 
         # Menambahkan cascading menu
         self.add_cascade(label="File", menu=menu_file)
