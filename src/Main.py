1e5b2c867660472c5827273962fe2ce06f567190
maulanawesome5 <maulanawesome5@gmail.com>
2023-12-05 19:47:52 +0700

    Layouting searchbar

--- src/Main.py
+++ src/Main.py
@@ -2,10 +2,6 @@ import tkinter as tk
 from myWidgets import menubar, mainframe
 
 
-selectionbar_color = "#EFF5F6"
-sidebar_color = "#F5E1FD"
-header_color = "#53366B"
-
 class App(tk.Tk):
     """Root widget, widget yang paling dasar pada aplikasi"""
 
@@ -32,6 +28,9 @@ class App(tk.Tk):
         # Pengaturan Layout sidebar saya taruh sini supaya bisa mendeteksi 
         # secara otomatis tinggi (height) resolusi layar
         self.mainframe.sidebar.place(x=0, y=0, width=300, height=self.winfo_height())
+
+        # # Widget Searchbar
+        self.mainframe.searchbar.place(x=300, y=0, width=self.winfo_width(), height=75)
 
         # # Function untuk menampilkan window aplikasi
         self.mainloop()
