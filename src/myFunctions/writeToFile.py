6e1ac6bd98be7fb4a619f843bb2f6ade7194e5a3
maulanawesome5 <maulanawesome5@gmail.com>
2023-12-07 10:47:51 +0700

    Program ini direvisi oleh Microsoft Bing

--- src/myFunctions/writeToFile.py
+++ src/myFunctions/writeToFile.py
@@ -4,19 +4,74 @@ import pandas as pd
 def exportDataFrameToFile(dataframe: pd.DataFrame, directory: str, ticker: str,
                           to_xlsx: bool = False, to_csv: bool = False):
     """
-    Fungsi kustom untuk mengekspor DataFrame menjadi Excel atau CSV.
+    Export a DataFrame to an Excel or CSV file.
+
+    Parameters
+    ----------
+    dataframe : pd.DataFrame
+        The DataFrame to be exported.
+    directory : str
+        The directory where the file will be stored.
+    ticker : str
+        The stock symbol of the DataFrame.
+    to_xlsx : bool, optional
+        Whether to export as Excel format. The default is False.
+    to_csv : bool, optional
+        Whether to export as CSV format. The default is False.
+
+    Returns
+    -------
+    None.
+
+    Raises
+    ------
+    Exception
+        If there is an error in writing the file.
     """
+
+    # Format the index as date string
     dataframe.index = dataframe.index.strftime("%Y-%m-%d")
 
-    if to_xlsx == True:
+    # Use f-string to create the file path
+    if to_xlsx and to_csv:
+        print("Please choose only one format: xlsx or csv")
+        return
+    elif to_xlsx:
         extension = ".xlsx"
+        engine = "openpyxl"
         writer = pd.ExcelWriter(
-            path=directory+ticker+extension,
-            engine="openpyxl",
+            path=f"{directory}{ticker}{extension}",
+            engine=engine,
             date_format="YYYY-MM-DD")
-        dataframe.to_excel(writer, sheet_name="_")
-        writer.close()
+        # Use try-except to handle errors
+        try:
+            dataframe.to_excel(writer, sheet_name="_")
+            writer.close()
+            print(
+                f"Successfully exported {ticker} data to {directory}{ticker}{extension}")
+        except Exception as e:
+            print(f"Error: {e}")
 
-    elif to_csv == True:
+    elif to_csv:
         extension = ".csv"
-        dataframe.to_csv(directory + ticker + extension, mode="w")
+        # Use try-except to handle errors
+        try:
+            dataframe.to_csv(f"{directory}{ticker}{extension}", mode="w")
+            print(
+                f"Successfully exported {ticker} data to {directory}{ticker}{extension}")
+        except Exception as e:
+            print(f"Error: {e}")
+
+# # Contoh penggunaan function buatan
+# 
+# import yfinance as yf
+# ticker = "BBRI.JK"
+
+# stock = yf.Ticker(ticker)
+# stock = stock.history(period="max", interval="1d")
+# df = pd.DataFrame(stock)
+
+# exportDataFrameToFile(dataframe=df, directory="data/",
+#                       ticker=ticker, to_xlsx=True)
+
+# print(stock)
