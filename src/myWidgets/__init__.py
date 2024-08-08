e17cae3443c5834cc7bf672245a229e61b1978c9
maulanawesome5 <maulanawesome5@gmail.com>
2024-08-09 06:17:04 +0700

    Hapus package kustom myWidgets

--- src/myWidgets/__init__.py
+++
@@ -1,7 +0,0 @@
-# # Tutorial import statement dengan __init__.py
-# # https://betterstack.com/community/questions/what-is-init-py-for/
-import myWidgets.chartframe
-import myWidgets.mainframe
-import myWidgets.menubar
-import myWidgets.searchbar
-import myWidgets.sidebar
